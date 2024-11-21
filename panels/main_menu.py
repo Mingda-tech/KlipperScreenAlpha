d8f776cd1a7541d6b4b473d4120e364fb9e89ae6
Sampson <Sampson@3dmingda.com>
2024-11-21 20:30:04 +0800

    bigfix: PID calibration, the Save button becomes Confirm

--- panels/main_menu.py
+++ panels/main_menu.py
@@ -210,7 +210,8 @@ class Panel(MenuPanel):
                 _("Initiate a PID calibration for:") + f" {self.active_heater} @ {temp} ºC"
                 + "\n\n" + _("It may take more than 5 minutes depending on the heater power."),
                 "printer.gcode.script",
-                script
+                script,
+                save_button=False
             )
 
     def create_left_panel(self):
